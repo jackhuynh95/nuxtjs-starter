e304f3136653adc0d1837cfbed717fe92f96617f
JackHuynh <truonghuynhxuan1995@gmail.com>
2021-03-10 16:03:56 +0700

    update styles and fonts

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,23 @@
 <template lang="pug">
   div
     h1 {{ ip }}
-    div(v-t='"header_menu.sign_in"')
+    .text-oswald(v-t='"header_menu.sign_in"')
     NuxtLink(:to='localePath("/", "en")') English
     NuxtLink(:to='localePath("/", "vi")') Viet Nam
     form(@submit.prevent='')
       input(v-model='text' name='text' v-validate='"required"')
       small.form-text.text-danger {{ veeErrors.first("text") }}
 
-</template>
+</template> 
+
+<style lang="stylus" scoped>
+@import '~@/styles/variables'
+
+div
+  color color-success
+
+</style>
+
 <script>
   import Vue from 'vue';
   import Component from 'nuxt-class-component';
