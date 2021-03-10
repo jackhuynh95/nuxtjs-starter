340eb0099c0a269a108d37e0812246e7ef3e6360
JackHuynh <truonghuynhxuan1995@gmail.com>
2021-03-10 11:23:02 +0700

    update init project

---
+++ nuxt.config.js
@@ -0,0 +1,61 @@
+export default {
+  server: {
+    port: 8080, // default: 3000
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'example-nuxt',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    '@nuxt/http',
+    'nuxt-element-ui',
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    babel: {
+      plugins: [
+        ["@babel/plugin-proposal-decorators", { "legacy": true }]
+      ]
+    },
+  },
+
+  elementUI: {
+    components: ['Select', 'Option'],
+    locale: 'en',
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  div
+    h1 {{ ip }}
+    //- el-select(v-model='ip')
+    //-   el-option(value='' label='1')
+    //-   el-option(value='2' label='2')
+    NuxtLink(to='/item/123') DETAIL
+
+</template>
+<script>
+  import Vue from 'vue';
+  import Component from 'nuxt-class-component';
+
+  @Component({})
+  export default class Home extends Vue {
+    isLoading = false;
+
+    mounted() {}
+
+    async asyncData ({ params, $http }) {
+      const ip = await $http.$get('https://icanhazip.com');
+      // text = ip;
+      return { ip };
+    }
+
+    head() {
+      return { 
+        title: this.ip,
+      };
+    }
+  }
+</script>
