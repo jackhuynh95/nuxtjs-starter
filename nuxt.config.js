80816c7f523a205e5611439f339dc7270cb468a1
JackHuynh <truonghuynhxuan1995@gmail.com>
2021-03-17 15:47:07 +0700

    update getScrollBehaviour

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,11 @@ export default {
   },
 
   router: {
-    middleware: ['auth']
+    middleware: ['auth'],
+    mode: 'history',
+    base: '/',
+    linkActiveClass: 'active',
+    scrollBehavior: getScrollBehaviour(),
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -174,4 +178,46 @@ export default {
       ],
     }
   },
+}
+
+function getScrollBehaviour() {
+  // scrollBehavior:
+  // - only available in html5 history mode
+  // - defaults to no scroll behavior
+  // - return false to prevent scroll
+  const scrollBehavior = function(to, from, savedPosition) {
+    if (to.path === from.path) {
+      return;
+    }
+
+    if (savedPosition) {
+      // savedPosition is only available for popstate navigations.
+      return savedPosition;
+    } else {
+      const position = {};
+
+      // scroll to anchor by returning the selector
+      if (to.hash) {
+        position.selector = to.hash;
+
+        // specify offset of the element
+        if (to.hash === '#anchor2') {
+          position.offset = { y: 100 };
+        }
+
+        if (document.querySelector(to.hash)) {
+          return position;
+        }
+
+        // if the returned position is falsy or an empty object,
+        // will retain current scroll position.
+        return false;
+      }
+      position.x = 0;
+      position.y = 0;
+      return position;
+    }
+  };
+
+  return scrollBehavior;
 }
