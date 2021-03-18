c776bb986db540c1c21d9786606afa0d7c4b4439
JackHuynh <truonghuynhxuan1995@gmail.com>
2021-03-18 10:40:40 +0700

    update account

--- apis/account.ts
+++ apis/account.ts
@@ -1,81 +1,5 @@
 import http from './http';
 
-export function registerStudent(
-  form: any,
-): Promise<any> {
-  return http
-  .callApi({
-    resource: 'account.registerStudent',
-    data: {
-      body: form,
-    },
-  })
-  .then(res => {
-    return res.data;
-  })
-  .catch(res => {
-    const error = (res as any).response.data;
-    return Promise.reject(error);
-  });
-}
-
-export function registerTutor(
-  form: any,
-): Promise<any> {
-  return http
-  .callApi({
-    resource: 'account.registerTutor',
-    data: {
-      body: form,
-    },
-  })
-  .then(res => {
-    return res.data;
-  })
-  .catch(res => {
-    const error = (res as any).response.data;
-    return Promise.reject(error);
-  });
-}
-
-export function loginStudent(
-  form: any,
-): Promise<any> {
-  return http
-  .callApi({
-    resource: 'account.loginStudent',
-    data: {
-      body: form,
-    },
-  })
-  .then(res => {
-    return res.data;
-  })
-  .catch(res => {
-    const error = (res as any).response.data;
-    return Promise.reject(error);
-  });
-}
-
-export function loginTutor(
-  form: any,
-): Promise<any> {
-  return http
-  .callApi({
-    resource: 'account.loginTutor',
-    data: {
-      body: form,
-    },
-  })
-  .then(res => {
-    return res.data;
-  })
-  .catch(res => {
-    const error = (res as any).response.data;
-    return Promise.reject(error);
-  });
-}
-
 export function getProfile(): Promise<any> {
   return http
     .callApi({
@@ -105,10 +29,6 @@ export function logout(): Promise<any> {
 }
 
 export default {
-  registerStudent,
-  registerTutor,
-  loginStudent,
-  loginTutor,
   getProfile,
   logout,
 };
